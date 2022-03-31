04e4e58150fdc45e386ea53e716a1dfcce2743d0
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-31 22:16:11 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,24 @@
+// Created by: Venika Sem
+// Created on: Mar 2022
+//
+// This program does calculations
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function calculates the area of a square
+        float sideValue;
+
+        Console.WriteLine("This program finds the area of a sqaure.");
+        Console.WriteLine("");
+        Console.WriteLine("Enter the side value (cm) : ");
+        sideValue = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("");
+        Console.WriteLine("The area of the square is " + ((sideValue * sideValue).ToString("0.00") + " " + "cm²."));
+
+        Console.WriteLine("\nDone.");
+    }
 }
